e73a7b3c2ff12551ce72b72acb9a649fa7eb81d5
Abel Sulca Espinoza <abelsulcaespinoza@gmail.com>
2025-02-19 18:50:17 -0500

    Version 1.0

---
+++ src/main/java/com/bd/cibertec/LpiiT3SulcaespinozaabelApplication.java
@@ -0,0 +1,13 @@
+package com.bd.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiT3SulcaespinozaabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiT3SulcaespinozaabelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bd/cibertec/LpiiT3SulcaespinozaabelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd.cibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpiiT3SulcaespinozaabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
